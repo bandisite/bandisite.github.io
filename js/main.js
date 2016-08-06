3d2344e75ac8e5aa4a8591ffbb30ea8f6a3bce68
VitalikSt <vitalik.sts@gmail.com>
2016-08-07 00:41:20 +0300

    inittial commit

--- js/main.js
+++ js/main.js
@@ -6,8 +6,16 @@ $(".owl-carousel").owlCarousel({
 });
 });
 /************//**************/
-
-/************MixItUp**************/
 $(function(){
 	$('.portfolio').mixItUp();
 });
+/************//**************/
+$(document).ready(function(){
+    $("nav ul li").on("click","a", function (event) {
+              event.preventDefault();
+     var id  = $(this).attr('href'),
+     top = $(id).offset().top;         
+    $('body,html').animate({scrollTop: top}, 1000);
+    });
+    })
+
